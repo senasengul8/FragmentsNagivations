80fe80b4c558dd9b082daea347d9ff0c4aa2ce30
Sena <44581008+senasengul8@users.noreply.github.com>
2024-02-20 00:54:27 +0300

    Update YFragment.kt

--- src/main/java/com/example/odev4/YFragment.kt
+++ src/main/java/com/example/odev4/YFragment.kt
@@ -1,11 +1,14 @@
 package com.example.odev4
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.odev4.databinding.FragmentXBinding
 import com.example.odev4.databinding.FragmentYBinding
 
@@ -19,6 +22,19 @@ class YFragment : Fragment() {
     ): View? {
         binding= FragmentYBinding.inflate(inflater, container, false)
         binding.button2.setOnClickListener{ Navigation.findNavController(it).navigate(R.id.action_YFragment_to_anasayfaFragment)}
+        val geriTusu=object: OnBackPressedCallback( true)
+        {
+            override fun handleOnBackPressed() {
+                if (findNavController().currentDestination?.id != R.id.anasayfaFragment) {
+                    // Navigate to the home fragment
+                    findNavController().navigate(R.id.action_YFragment_to_anasayfaFragment3 )
+                } else {
+                }
+                ///geridonusaktifdegil
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,geriTusu)
+
         // Inflate the layout for this fragment
         return binding.root
     }
